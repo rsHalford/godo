ae23f0adbf8e5d5453ed537c13f16349ae0d77be
Richard Halford <dev@rshalford.com>
2021-05-26 13:33:58 +0100

    🐛 v0.2.1 - fixed configuration filepath bug. Now correctly looks under XDG_CONFIG_HOME/godo/config.yaml

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,7 @@ var versionCmd = &cobra.Command{
 	Aliases: []string{"v"},
 	Short:   "print godo's version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.2.0")
+		fmt.Println("v0.2.1")
 	},
 }
 
