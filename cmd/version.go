9701f8e9dcadb9939e343025bb77577d647893b4
Richard Halford <dev@rshalford.com>
2021-10-17 23:50:34 +0100

    👕 v0.7.0 - added flags for get (-b/--body) and find (-t/--title), and cleaned up variables calls for remote api

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,7 @@ var versionCmd = &cobra.Command{
 	Aliases: []string{"v"},
 	Short:   "print godo's version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.6.1")
+		fmt.Println("v0.7.0")
 	},
 }
 
