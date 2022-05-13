197fe958d2a20da0d4a055c0e33dfbda0895547f
Richard Halford <dev@rshalford.com>
2022-05-13 12:53:58 +0100

    chore(version): update for v0.12.2

--- cmd/version.go
+++ cmd/version.go
@@ -28,7 +28,7 @@ var versionCmd = &cobra.Command{
 	Aliases: []string{"v"},
 	Short:   "print godo's version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.12.1")
+		fmt.Println("v0.12.2")
 	},
 }
 
