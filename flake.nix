8a97788b275ed3b5e3b8ac9abb0d960ac08150d2
Richard Halford <dev@rshalford.com>
2022-06-23 19:35:36 +0100

    chore(version): update for v0.15.0

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      version = "0.14.0";
+      version = "0.15.0";
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
